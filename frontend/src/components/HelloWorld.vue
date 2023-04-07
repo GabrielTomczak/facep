6bfe1562f78a3932cae35d42f9a854a626bf1cda
Lucas dos Anjos <42100521+LucasAnjs@users.noreply.github.com>
2023-04-07 14:09:07 -0300

    Criação do repositório e front-end do projeto.

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>Software de Biomedicina Estética.</h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+.greetings h1 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1 {
+    text-align: center;
+  }
+}
+</style>
